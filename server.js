9feec58d0fde3934aba3179855a9fe0dfb9323d5
wheatley1239 <wickselm2005@gmail.com>
2022-03-30 10:34:34 +0100

    setup favicon

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const path = require("path")
 const app = express();
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
 
-    res.send("Hello World");
+    res.sendFile(path.join(__dirname, "index.html"));
 
 });
 
