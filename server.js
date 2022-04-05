3401f0bba67168e9c3af3c982542c3d11bc2489a
wheatley1239 <wickselm2005@gmail.com>
2022-04-05 12:35:53 +0100

    created subject page

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const path = require("path")
 const app = express();
 
 app.use(express.static("public"));
-app.use(express.static("account"));
+app.use(express.static(".account"));
+app.use(express.static(".subjects"));
 
 app.get("/", (req, res) => {
+
+    res.redirect("/home");
+
+});
+
+app.get("/home", (req, res) => {
 
     res.sendFile(path.join(__dirname, "index.html"));
 
@@ -13,13 +20,19 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
 
-    res.sendFile(path.join(__dirname + "/account/login.html"));
+    res.sendFile(path.join(__dirname + "/.account/login.html"));
 
 });
 
 app.get("/forgot-pass", (req, res) => {
 
-    res.sendFile(path.join(__dirname + "/account/forgot.html"));
+    res.sendFile(path.join(__dirname + "/.account/forgot.html"));
+
+});
+
+app.get("/subjects", (req, res) => {
+
+    res.sendFile(path.join(__dirname + "/.subjects/subjects.html"));
 
 });
 
